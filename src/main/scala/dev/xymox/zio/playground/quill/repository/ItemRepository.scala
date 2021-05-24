5ae92761b9e656a52e5445246d43e495c5b3b7c1
Terry Drozdowski <terry.drozdowski@icloud.com>
2021-05-24 00:12:52 -0700

    repository clean up

--- src/main/scala/dev/xymox/zio/playground/quill/repository/ItemRepository.scala
+++ src/main/scala/dev/xymox/zio/playground/quill/repository/ItemRepository.scala
@@ -1,7 +1,7 @@
 package dev.xymox.zio.playground.quill.repository
 
 import io.getquill.context.ZioJdbc.{QConnection, QDataSource}
-import zio.{RIO, Task, ZLayer}
+import zio._
 import zio.blocking.Blocking
 
 trait ItemRepository {
@@ -12,7 +12,13 @@ trait ItemRepository {
 
 object ItemRepository {
 
-  val zioConn: ZLayer[Blocking, Throwable, QConnection] =
-    QDataSource.fromPrefix("zioQuillExample") >>> QDataSource.toConnection
+  def create(item: ItemRecord): RIO[Has[ItemRepository], ItemRecord] = ZIO.serviceWith[ItemRepository](_.create(item))
+  def all: RIO[Has[ItemRepository], Seq[ItemRecord]]                 = ZIO.serviceWith[ItemRepository](_.all)
+  def findById(id: Int): RIO[Has[ItemRepository], ItemRecord]        = ZIO.serviceWith[ItemRepository](_.findById(id))
+
+  val zioConn: TaskLayer[QDataSource] =
+    ZEnv.live >>> QDataSource.fromPrefix("zioQuillExample")
+
+  // live layer
 
 }
