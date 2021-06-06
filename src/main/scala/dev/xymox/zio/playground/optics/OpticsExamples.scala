52711b4237835b7d25121cad7bd0d3635c0f2b73
Terry Drozdowski <terry.drozdowski@icloud.com>
2021-06-05 22:15:45 -0700

    some (updated) optics examples

--- src/main/scala/dev/xymox/zio/playground/optics/OpticsExamples.scala
+++ src/main/scala/dev/xymox/zio/playground/optics/OpticsExamples.scala
@@ -26,22 +26,21 @@ object Item {
 
 object OpticsExamples extends App {
 
-  val items: Seq[Item] = Seq(
+  val items: Chunk[Item] = Chunk(
     Item(1, "Boomstick", "This..is my boomstick!", 255.50, Instant.now),
     Item(2, "Helm", "A rather cheap looking helm", 19.99, Instant.now)
   )
 
-  // calcualte a 10% discount for each item and update it
-  val tenPercentDiscount: Seq[Item] =
-    items.map(item => Item.price.update(item)(price => Item.calcDiscount(price))).map(_.getOrElse(throw new Exception("Shouldn't happen")))
+  def hasPrice(item: Item) = item.price > 0.00
 
-  def printItems(items: Seq[Item]) = items.map(item => (Item.name.get(item), Item.price.get(item)))
+  def discountItems: Traversal[Chunk[Item], Double] =
+    Optic.filter(hasPrice).foreach(Item.price)
 
   val program: ZIO[Console, Throwable, Unit] =
     for {
       _       <- putStrLn(s"All items: \n\t ${items.map(i => (i.name, i.price)).mkString(", \n\t ")}")
       _       <- putStrLn("Applying 10% discount to all items!")
-      updated <- ZIO.effect(tenPercentDiscount)
+      updated <- ZIO.fromEither(discountItems.update(items)(_.map(Item.calcDiscount)))
       _       <- putStrLn(s"All items discounted: \n\t ${updated.map(i => (i.name, i.price)) mkString (", \n\t ")}")
     } yield ()
 
@@ -55,14 +54,18 @@ object OpticsWithRefExample extends App {
     Item(2, "Helm", "A rather cheap looking helm", 19.99, Instant.now)
   )
 
-  def discountItems(items: Chunk[Item]): Chunk[Item] = items.map(item => Item.price.update(item)(price => Item.calcDiscount(price))).map(_.toOption.get)
+  def hasPrice(item: Item) = item.price > 0.00
 
-  val program: ZIO[Console, IOException, Unit] =
+  def discountItems: Traversal[Chunk[Item], Double] =
+    Optic.filter(hasPrice).foreach(Item.price)
+
+  val program: ZIO[Console, Exception, Unit] =
     for {
-      ref <- Ref.make(items)
-      _   <- putStrLn(s"Wrapped items in ref: $ref")
-      _   <- ref.update(discountItems)
-      _   <- putStrLn(s"After update: $ref")
+      ref     <- Ref.make(items)
+      _       <- putStrLn(s"Wrapped items in ref: $ref")
+      updated <- ZIO.fromEither(discountItems.update(items)(_.map(Item.calcDiscount)))
+      _       <- ref.set(updated)
+      _       <- putStrLn(s"After update: $ref")
     } yield ()
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = program.exitCode
