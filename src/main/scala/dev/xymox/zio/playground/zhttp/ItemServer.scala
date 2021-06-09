431eb64ca3009ef625ca955eae43d4eebb6575cc
Terry Drozdowski <terry.drozdowski@icloud.com>
2021-06-08 19:19:05 -0700

    added CORS

--- src/main/scala/dev/xymox/zio/playground/zhttp/ItemServer.scala
+++ src/main/scala/dev/xymox/zio/playground/zhttp/ItemServer.scala
@@ -12,7 +12,10 @@ import zio.magic._
 object ItemServer extends App {
 
   val endpoints: Http[Has[ItemService] with Console, HttpError, Request, Response[Has[ItemService] with Console, HttpError]] =
-    AuthenticationApp.login +++ AuthenticationApp.authenticate(HttpApp.forbidden("None shall pass."), ItemEndpoints.item)
+    AuthenticationApp.login +++ CORS(
+      AuthenticationApp.authenticate(HttpApp.forbidden("None shall pass."), ItemEndpoints.item),
+      config = CORSConfig(anyOrigin = true)
+    )
 
   val program: ZIO[Any, Throwable, Nothing] = Server
     .start(8080, endpoints)
