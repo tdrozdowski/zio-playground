537b91c526b4fd0296c2d85a0bf4cf3eacff4185
tdrozdow <terrence.drozdowski@carvana.com>
2021-08-16 16:44:02 -0700

    final changes to get redoc to work with zio-http

--- src/main/scala/dev/xymox/zio/playground/tapir/TapirItemServer.scala
+++ src/main/scala/dev/xymox/zio/playground/tapir/TapirItemServer.scala
@@ -1,18 +1,21 @@
 package dev.xymox.zio.playground.tapir
 
+import dev.xymox.zio.playground.quill.ZioQuillContext
+import dev.xymox.zio.playground.zstack.repository.ItemRepository
 import dev.xymox.zio.playground.zstack.service.item.ItemService
 import zhttp.http.{Http, Request, Response}
 import zhttp.service.Server
 import zio._
+import zio.console.Console
 import zio.magic._
 
 object TapirItemServer extends App {
 
-  val endpoints: Http[Any, Throwable, Request, Response[Any, Throwable]] = TapirItemEndpoints.zhttpApp +++ ItemDocs.redocApp
+  val endpoints: Http[Has[ItemService], Throwable, Request, Response[Has[ItemService], Throwable]] = ItemDocs.redocApp +++ TapirItemEndpoints.zhttpApp
 
   val program: ZIO[Any, Throwable, Nothing] = Server
     .start(8080, endpoints)
-    .inject(ItemService.layer)
+    .inject(Console.live, ZioQuillContext.dataSourceLayer, ItemRepository.layer, ItemService.layer)
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = program.exitCode
 }
